27909f96ae7ebfcbd8fc3580661d1e20313ad02f
garima94921 <gaarimabothra@gmail.com>
2020-06-20 13:50:59 +0530

    Initial Commit

---
+++ RecogX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecogX
+//
+//  Created by Garima Bothra on 20/06/20.
+//  Copyright © 2020 Garima Bothra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
