6ed02208704badf801aab790c725fa06c08f1037
garima94921 <gaarimabothra@gmail.com>
2020-06-21 03:03:00 +0530

    Fix: Signup page UI

--- RecogX/Controller/ViewController.swift
+++ RecogX/Controller/ViewController.swift
@@ -11,9 +11,14 @@ import GoogleSignIn
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ourMissionLabel: UILabel!
+    @IBOutlet weak var googleButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         GIDSignIn.sharedInstance()?.presentingViewController = self
+        googleButton.layer.cornerRadius = 10
+        ourMissionLabel.text = "🦄 Development: Help you make your mark in the tech field \n\n 🦄 Inclusion: Everyone matters and matters equally. \n\n 🦄 Empowerment: Finding opportunities for all and help them grow in tech. \n\n 🦄 Diversity: Our differences spark innovation and bringing everyone together under the same roof.  \n\n 🦄 Making a difference: Making a positive change in the world with access to opportunity "
         // Do any additional setup after loading the view.
     }
 
