6fe7c7803c347866379e08cb465dd77ac8f11515
garima94921 <gaarimabothra@gmail.com>
2020-06-20 17:41:48 +0530

    Feat. User Form

--- RecogX/Controller/UserFormViewController.swift
+++ RecogX/Controller/UserFormViewController.swift
@@ -8,14 +8,60 @@
 
 import UIKit
 
-class UserFormViewController: UIViewController {
+class UserFormViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+
+    @IBOutlet weak var nameTextfield: UITextField!
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var callTextField: UITextField!
+    @IBOutlet weak var genderTextField: UITextField!
+    @IBOutlet weak var dropDown: UIPickerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        genderTextField.delegate = self
+        dropDown.delegate = self
+        dropDown.dataSource = self
+        self.dropDown.isHidden = true
+        self.navigationItem.title = "About You"
+        self.navigationItem.largeTitleDisplayMode = .always
         // Do any additional setup after loading the view.
     }
-    
+
+    var list = ["Male", "Female", "Not specified"]
+
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+
+        return list.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+
+       // self.view.endEditing(true)
+        return list[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+
+        self.genderTextField.text = self.list[row]
+        self.dropDown.isHidden = true
+        self.genderTextField.endEditing(true)
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+
+        if textField == self.genderTextField {
+            self.dropDown.reloadAllComponents()
+            self.dropDown.isHidden = false
+            self.dropDown.reloadAllComponents()
+            //if you don't want the users to se the keyboard type:
+
+            textField.endEditing(true)
+        }
+    }
 
     /*
     // MARK: - Navigation
